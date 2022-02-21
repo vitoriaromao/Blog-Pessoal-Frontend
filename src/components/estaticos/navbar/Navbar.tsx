cfa4615de8bd5029bb3873fee21daab3edcd77f2
vitoria romao <vitoria.rmoura2@hotmail.com>
2022-02-21 17:32:06 -0300

    Adicionando o cadasto no front

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+
 function Navbar() {
     return (
         <>
